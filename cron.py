7471c8ded6cf6ada57958f51d2a4f9946d1749bc
fredkingham <fredkingham@gmail.com>
2018-01-10 13:40:29 +0000

    fix up the cron job to run cd

--- cron.py
+++ cron.py
@@ -1,6 +1,9 @@
 from crontab import CronTab
 from fabric.api import env
 from common import lexists
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class Cron(object):
@@ -11,8 +14,8 @@ class Cron(object):
     @classmethod
     def setup_backup(cls):
         # currently using root, not nice but we su into postgres
-        fabric_virtual_env = "workon elcid-setup && /home/{0}/.virtualenvs/elcid-setup/bin/fab"
-        fabric_virtual_env = fabric_virtual_env.format(env.nix_user)
+        fabric_virtual_env = "cd {0} && /home/{1}/.virtualenvs/elcid-setup/bin/fab"
+        fabric_virtual_env = fabric_virtual_env.format(BASE_DIR, env.nix_user)
         comment = cls.get_comment()
         crontab = CronTab(user=True)
         crontab.remove_all(comment=comment)
